d6f1f1691178ce21cb48ee3a29c55fdac1b4add7
krinconme <85189409+krinconme@users.noreply.github.com>
2021-06-01 12:52:01 -0500

    Código Kevin
    Código Kevin

--- Main.java
+++ Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args){
         System.out.println ("Hello World");
         System.out.println ("Hello World2");    
-        System.out.println ("Editado por Lizo");
         
         int var1 = 15; 
         int var2 = 20; 
         
         System.out.println(var1 + var2);
+        
+        String name = "Kevin";
+        System.out.print(name.toUpperCase());
+        
+        
     }
 }
