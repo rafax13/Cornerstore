1aaffdabe4555b9f2c8b56c702d25d37042b4688
tatianagb1997 <85189541+tatianagb1997@users.noreply.github.com>
2021-06-01 12:49:50 -0500

    Actualización Tatiana
    Se agregaron 3 líneas de código nuevo.

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args){
         System.out.println ("Hello World");
         System.out.println ("Hello World2");    
+        
+        int var1 = 15; 
+        int var2 = 20; 
+        
+        System.out.println(var1 + var2);
     }
 }
